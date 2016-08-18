594b0631f8b921b865d150ad4528ac426bddb1d5
Baptiste LENIAU <baptiste.leniau@subatech.in2p3.fr>
2016-08-18 12:13:21 +0200

    Make the code prettier with additional comments + change routes name + add better error handling

--- app.js
+++ app.js
@@ -1,45 +1,49 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var swig = require('swig');
-var logger = require('morgan');
+
+/**************** APP STATUS  *********************/
+
+process.env.NODE_ENV = 'development';
+
+/**************** DEPENDANCIES *********************/
+//npm modules
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var cons         = require('consolidate')
 var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+//my modules
+var router      = require('./routes/api');
 
+/**************** The app *********************/
 var app = express();
 
+/**************** FAVICON *********************/
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
+
+/*********** VIEW RENDING *********************/
 // view engine setup
-// utilisation du moteur de swig pour les .html
-app.engine('html', swig.renderFile); 
-// utiliser le moteur de template pour les .html
+// use swig engine for .html
+app.engine('html', cons.swig); 
+// use template engine pour les .html
 app.set('view engine', 'html'); 
 // dossier des vues
 app.set('views', path.join(__dirname, 'views')); 
-
 // view cache
 app.set('view cache', false); // désactivation du cache express
-swig.setDefaults({ cache: false }); // désactivation du cache swig
 
 
-// uncomment after placing your favicon in /public
-
-app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
-app.use(logger('dev'));
-
-var bodyParser = require('body-parser');
+/**************** PARSERS *********************/
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.use('/', routes);
-app.use('/users', users);
+/**************** ROUTES *********************/
+app.use('/api', router);
 
-
+/**************** ERROR HANDLING *************/
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -47,30 +51,29 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
-
 // development error handler
 // will print stacktrace
-/*
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render('exceptions/errors', {
       message: err.message,
-      error: err
+      error: err + ""
     });
   });
 }
-
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render('exceptions/errors', {
     message: err.message,
     error: {}
   });
 });
-*/
 
+/*************** EXPORT APP **************/
 module.exports = app;
+
+
+
